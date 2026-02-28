0866216d986517a2497f51283ee7461281479ee1
Dawid Szymik <dawid_szymik@wp.pl>
2026-02-28 16:56:16 +0100

    Add ProductLabel model and update project file
    Added ProductLabel.cs with an internal ProductLabel class containing Id, Name, Ingredients, and ImagePath properties. Also removed explicit "Model" folder inclusion from LabelController.csproj.

---
+++ Model/ProductLabel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabelController.Model {
+    internal class ProductLabel {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Ingredients { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
